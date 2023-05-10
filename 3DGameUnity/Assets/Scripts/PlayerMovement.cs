001da56c9b55d17aac852715db8fcada9efcb219
MagicMule <102788198+MagicMule@users.noreply.github.com>
2023-05-10 14:25:07 +0200

    Added ground check

--- 3DGameUnity/Assets/Scripts/PlayerMovement.cs
+++ 3DGameUnity/Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
     public float moveSpeed;
+
+    public float groundDrag;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGrund;
+    bool grounded;
 
     public Transform orientation;
 
@@ -24,7 +32,17 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        // ground check
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight);
+
         MyInput();
+        SpeedControl();
+
+        // handle drag
+        if (grounded)
+            rB.drag = groundDrag;
+        else
+            rB.drag = 0;
     }
 
     private void FixedUpdate()
@@ -49,5 +67,17 @@ public class PlayerMovement : MonoBehaviour
         moveDirection = ( orientation.forward * verticalInput ) + ( orientation.right * horizontalInput );
 
         rB.AddForce(10 * moveSpeed * moveDirection.normalized, ForceMode.Force);
+    }
+
+    // Manualy cotrol of speed
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rB.velocity.x, 0f, rB.velocity.z);
+
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rB.velocity = new Vector3(limitedVel.x, rB.velocity.y, limitedVel.z); // limit velocity on x and z
+        }
     }
 }
