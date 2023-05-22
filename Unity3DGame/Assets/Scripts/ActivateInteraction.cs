77914cc68f50fde36fecce2121672f4ac5dfb620
MagicMule <102788198+MagicMule@users.noreply.github.com>
2023-05-22 10:45:19 +0200

    Basic interaton
    Can now left clik to spawn "interactioncone"

---
+++ Unity3DGame/Assets/Scripts/ActivateInteraction.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class ActivateInteraction : MonoBehaviour
+{
+    [Header("GameObjekt used to initaite interaction")]
+    public GameObject interactor;
+
+    [Header("Interaction")]
+    public bool interactReady = true;
+    public float interactDeley = 0.5f; // time befor next interaction bekoms avialable
+    public float interactDuration = 0.2f; // active time of iteractor objekt
+    public KeyCode interactKey = KeyCode.Mouse0;
+
+    private void Update()
+    {
+        StartInteraction();
+    }
+
+    void StartInteraction()
+    {
+        if (Input.GetKey(interactKey) && interactReady)
+        {
+            interactor.SetActive(true);
+            StartCoroutine(DoInteraction());
+        }
+    }
+
+    IEnumerator DoInteraction()
+    {
+        interactReady = false;
+
+        yield return new WaitForSeconds(interactDuration);
+
+        interactor.SetActive(false);
+
+        StartCoroutine(DelayInteraction()); // Start deley
+    }
+
+    IEnumerator DelayInteraction()
+    {
+        yield return new WaitForSeconds(interactDeley);
+        interactReady = true;
+    }
+
+}
