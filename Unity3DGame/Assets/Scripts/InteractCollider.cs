ea0d3d6476251b329a43edb5878c6df1b9c372f8
MagicMule <102788198+MagicMule@users.noreply.github.com>
2023-05-26 10:42:45 +0200

    Dungon, Inputhandeler

--- Unity3DGame/Assets/Scripts/InteractCollider.cs
+++ Unity3DGame/Assets/Scripts/InteractCollider.cs
@@ -5,26 +5,33 @@ using UnityEngine;
 
 public class InteractCollider : MonoBehaviour
 {
+    /// <summary>
+    /// This script is on the InteractCylinder gameobjekt
+    /// The following are interactions that follow the gameobjekts colltion
+    /// </summary>
+
     [Header("UI and general Objekt to cange by interaction")]
     public GameObject InteractUI;
-    public bool InteractUiIsClosed = true;
+    public bool InteractUIIsClosed = true;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("NPC") && InteractUiIsClosed)
+        // Player interact with npc
+        if (other.gameObject.CompareTag("NPC") && InteractUIIsClosed)
         {
             InteractUI.SetActive(true); // Open UI when colion with npc
-            InteractUiIsClosed = false; // UI is open -> set InteractUiclosed to false
+            InteractUIIsClosed = false; // UI is open -> set InteractUIClosed to false
         }
-        else if (other.gameObject && !InteractUiIsClosed)
+        else if (other.gameObject && !InteractUIIsClosed)
         {
-            InteractUI.SetActive(false); // Close Ui when player activates InteractUI agin (Must hit a gameObjekt)
-            InteractUiIsClosed = true;
+            InteractUI.SetActive(false); // Close Ui when player activates InteractUI again (Must hit a gameObjekt)
+            InteractUIIsClosed = true; // UI is closed -> set interactUIClosed to True
         }
 
         // Player hit enemy
         if (other.gameObject.CompareTag("Enemy"))
         {
             Debug.Log("HIT AND ENEMY!");
+            Destroy(other.gameObject);
         }
         Debug.Log("Interakting with " + other.gameObject.name);
     }
