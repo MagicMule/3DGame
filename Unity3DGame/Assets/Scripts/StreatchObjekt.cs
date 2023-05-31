038120436c0a5d9dd140225e196d2f3934621713
MagicMule <102788198+MagicMule@users.noreply.github.com>
2023-05-31 14:15:57 +0200

    Expand and shrink

--- Unity3DGame/Assets/Scripts/StreatchObjekt.cs
+++ Unity3DGame/Assets/Scripts/StreatchObjekt.cs
@@ -7,24 +7,61 @@ public class StreatchObjekt : MonoBehaviour
     public Vector3 targetScale = new Vector3(10f, 1f, 1f);
     public float scaleSpeed = 1f;
 
+    public bool expand;
+
     private void Update()
     {
-        // Calculate the new scale based on the target scale and speed
-        // localScale incres every update, scaleSpeed decreses the targetScale totoal making so more exextuons are nesesary to reace target 
-
-        Vector3 newScale = transform.localScale + targetScale * scaleSpeed * Time.deltaTime;
-        
-
-        // Check if the new scale has reached or exceeded the target scale
-        if (newScale.x >= targetScale.x && newScale.y >= targetScale.y && newScale.z >= targetScale.z)
-        {
-            //Stop expanding
-            Debug.Log("Stop expanding");
-        }
-        else
-        {
-            // Update the scale
-            transform.localScale = newScale;
-        }
+        ExpandObjektScale();
+        ShrinkObjakteScale();
     }
+
+    void ExpandObjektScale()
+    {
+        if(expand == true)
+        {
+            // Calculate the new scale based on the target scale and speed
+            // localScale incres every update, scaleSpeed decreses the targetScale totoal making so more exextuons are nesesary to reace target 
+            // Increas only the scale in x
+
+            Vector3 newScale = transform.localScale + scaleSpeed * Time.deltaTime * new Vector3(targetScale.x, 0, 0);
+
+
+            // Check if the new scale has reached or exceeded the target scale
+            if (newScale.x >= targetScale.x && newScale.y >= targetScale.y && newScale.z >= targetScale.z)
+            {
+                //Stop expanding
+                Debug.Log("Stop expanding");
+            }
+            else
+            {
+                // Update the scale
+                transform.localScale = newScale;
+            }
+        }
+
+    }
+
+    void ShrinkObjakteScale()
+    {
+        if (expand == false)
+        {
+            Vector3 newScale = transform.localScale - (1 / scaleSpeed) * Time.deltaTime * new Vector3(targetScale.x, 0, 0);
+
+
+            // Check if the new scale has reached or exceeded the target scale
+            if (newScale.x <= targetScale.x && newScale.y <= targetScale.y && newScale.z <= targetScale.z)
+            {
+                //Stop expanding
+                Debug.Log("Stop shrinking");
+            }
+            else
+            {
+                // Update the scale
+                transform.localScale = newScale;
+            }
+        }
+
+
+    }
+
 }
