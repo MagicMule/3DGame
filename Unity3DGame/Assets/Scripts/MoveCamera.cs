7504ae5a1bd1cb5170b0d67c7ee61eb531b9043b
MagicMule <102788198+MagicMule@users.noreply.github.com>
2023-06-02 14:27:21 +0200

    Camera ofset

--- Unity3DGame/Assets/Scripts/MoveCamera.cs
+++ Unity3DGame/Assets/Scripts/MoveCamera.cs
@@ -1,15 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MoveCamera : MonoBehaviour
 {
     public Transform cameraPosition;
-    public Transform cameraRotation;
 
     private void FixedUpdate()
     {
         // update the camera positon to applied objekts transform 
-        transform.position = cameraPosition.position;
+        transform.position = new Vector3(cameraPosition.position.x, cameraPosition.position.y + 0.5f, cameraPosition.position.z);
     }
 }
