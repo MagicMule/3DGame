93be8cd7b56fc00484d557a5aa14f17d56ac8922
MagicMule <102788198+MagicMule@users.noreply.github.com>
2023-07-17 14:17:08 +0200

    MeshMonitor works

--- Unity3DGame/Assets/Game/Scripts/Camera/MeshMonitor.cs
+++ Unity3DGame/Assets/Game/Scripts/Camera/MeshMonitor.cs
@@ -1,25 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 
 public class MeshMonitor : MonoBehaviour
 {
     Transform playerCamTransform; // Stores the FPS camera transform
     private bool visible = true;
-    private float distanceToAppear = 100;
-    Renderer[] objRenderer;
-
-    GameObject[] childObjekts;
+    private float distanceToAppear = 50;
+    List<Renderer> objRenderer;
 
     private void Start()
     {
-        playerCamTransform = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<Camera>().transform;//Get camera transform reference
-        objRenderer = gameObject.GetComponentsInChildren<Renderer>(); //Get render reference
+        playerCamTransform = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<Camera>().transform; // Get camera transform reference
+        UpdateRendererReferences(); // Initial update of renderer references
     }
+
     private void Update()
     {
         DisappearChecker();
     }
+
     private void DisappearChecker()
     {
         float distance = Vector3.Distance(playerCamTransform.position, transform.position);
@@ -31,21 +32,40 @@ public class MeshMonitor : MonoBehaviour
             {
                 foreach (Renderer renderer in objRenderer)
                 {
+                    if (renderer != null)
+                    {
+                        renderer.enabled = true; // Show Object
+                    }
 
-
-                    renderer.enabled = true; // Show Object
-                    visible = true;
-                    Debug.Log("Visible");
                 }
+                visible = true;
+                Debug.Log("Visible");
             }
         }
         else if (visible)
         {
             foreach (Renderer renderer in objRenderer)
             {
-                renderer.enabled = false; // Hide Object
-                visible = false;
-                Debug.Log("InVisible");
+                if (renderer != null)
+                {
+
+                    renderer.enabled = false; // Hide Object
+                }
+            }
+            visible = false;
+            Debug.Log("InVisible");
+        }
+    }
+
+    // Call this method after re-enabling the object to update the references
+    private void UpdateRendererReferences()
+    {
+        objRenderer = new List<Renderer>();
+        foreach (Renderer renderer in GetComponentsInChildren<Renderer>())
+        {
+            if (renderer != null)
+            {
+                objRenderer.Add(renderer);
             }
         }
     }
