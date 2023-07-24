e370ed2356f8b0d3aee7d94648cbbf9c99ccc9e5
MagicMule <102788198+MagicMule@users.noreply.github.com>
2023-07-24 14:28:52 +0200

    Update InteractCollider.cs

--- Unity3DGame/Assets/Game/Scripts/TriggerAndInput/InteractCollider.cs
+++ Unity3DGame/Assets/Game/Scripts/TriggerAndInput/InteractCollider.cs
@@ -37,6 +37,7 @@ public class InteractCollider : MonoBehaviour
         // Player hit enemy
         if (other.gameObject.CompareTag("Enemy"))
         {
+            //other.gameObject.GetComponent
             other.gameObject.GetComponent<Enemy>().HP = GameManager.Instance.DecreaseHP(other.gameObject.GetComponent<Enemy>().HP, 1); // hit enemy with 1 point of damage
 
             if (other.gameObject.GetComponent<Enemy>().HP <= 0) // Destory enemy
