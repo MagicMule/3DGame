4a48e77a7bf0450e7b57b26813b8ae13b8e00b4d
MagicMule <102788198+MagicMule@users.noreply.github.com>
2023-12-05 10:42:34 +0100

    Setup To push

--- Unity3DGame/Assets/Scripts/Dialog/DialogText.cs
+++ Unity3DGame/Assets/Scripts/Dialog/DialogText.cs
@@ -126,9 +126,9 @@ public class DialogText : MonoBehaviour
         dilogLinesG.line.Add("I know only you were gone when I entered this place. My reverence for Odion extends to you.");
         dilogLinesG.line.Add("I was made a prisoner by agents of the Demon. It was not of my choosing.");
         dilogLinesG.line.Add("You are not a damsel. What manner made you so?");
-        dilogLinesG.line.Add("Do not jest me mage! There is no enemy I can not face! I pierce the mountain! I pierce the heart of gods! I pearce the inner logic! I pearce the thought-stuf of tomorrow and the future! But. This place and its demons have weekend me, for the Demon has acolytes of the dark spot, scum of the enemy.");
+        dilogLinesG.line.Add("Do not jest me mage! There is no enemy I can not face! I pierce the mountain! I pierce the heart of the giant! I pearce the inner logic! I pearce the thought-stuf of tomorrow and the future! But. This place and its evils have weekend me, for the Tog-agl has acolytes of the dark spot, scum of the enemy.");
         dilogLinesG.line.Add("… That is a possibility I tried not to consider. But it looks true. Come to me again. I need your grand force to break out of this library.");
-        dilogLinesG.line.Add("I saw where you entered. On the same low ground, at the end of the complex, there is rubble and instability. I know for true that my power is grand, that it is to clear a path there. Let's break forth.");
+        dilogLinesG.line.Add("I saw where you entered. On the same low ground, at the end of the complex, there is rubble and instability. I know for true that my power is grand, that it is to clear a path there. Let's break forth!!");
     }
     void Interactives()
     {
